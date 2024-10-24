7d566e7a283ed0ed3c190ebce31b27cfdf6f67ab
Dumindu Charuka <duminduabcd@gmail.com>
2024-10-24 21:27:19 +0530

    update routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router,Route, Routes} from "react-router-dom"
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Phototours from './components/phototours/phototours';
+import Lessons from "./components/Lessons/lessons";
+import Contact from './components/contact/contact';
+import Collections from './components/collections/collections';
+import Blog from './components/blog/blog';
+import Alex from "./components/Alex/Alex";
+import Panoramic from "./components/panoramic/panoramic";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <div>
+      <Header></Header><br></br><br></br><br></br>
+    <Router>
+         <Routes>
+              <Route exact path="/phototours" element={<Phototours/>}/> 
+              <Route exact path="/lessons" element={<Lessons/>}/>
+              <Route exact path="/contact" element={<Contact/>}/>
+              <Route exact path="/collections" element={<Collections/>}/>
+              <Route exact path="/blog" element ={<Blog/>}/>
+              <Route exact path="/alex" element={<Alex/>}/>
+              <Route exact path="/panoramic" element={<Panoramic/>}/>
+              <Route exact path="/home" element={<Home/>}/>
+              <Route path="/" element={<Home/>}/>
+         </Routes>
+    </Router>
+    
+  </div>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import './Header.css'
 import Logo from '../../assets/Logo.png'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-        <img src={Logo} alt='' className='logo'/>
+        <a href="home" className="dropbtn"><img src={Logo} alt='' className='logo'/></a>
         <ul className='header-menu'>
-            <li>Home</li>
-            <li>Alex's vision</li>
-            <li>1'ever panoramic landscapes tutorial</li>
-            <li>collections</li>
-            <li>phototours</li>
-            <li>editing lessons</li>
-            <li>blog</li>
-            <li>contact</li>
+            <li><a href="home">Home</a></li>
+            <li><a href="alex">Alex's vision</a></li>
+            <li><a href="panoramic">1'ever panoramic landscapes tutorial</a></li>
+            <li><a href="collections">collections</a></li>
+            <li><a href="phototours">phototours</a></li>
+            <li><a href="lessons">editing lessons</a></li>
+            <li><a href="blog">blog</a></li>
+            <li><a href="contact">contact</a></li>
         </ul>
     </div>
   )
